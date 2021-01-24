185290f2b5d98a4d0c157f1a02b0dcbb21eb4e1b
markplitt <markplitt@gmail.com>
2021-01-24 15:23:36 -0800

    speed column in vr_align_2p

--- sess.py
+++ sess.py
@@ -92,19 +92,19 @@ class SessionInfo:
             # check that provided keys are the right type
             warnings.warn("skipping checking keys, remaining initialization not guaranteed to work")
 
-        # check for VR data
-        if self.vr_filename is None:
-            self._check_for_VR_data()
-
-        if not self.VR_only:
-            # check for raw 2P data
-            self._check_for_2P_data()
-
-            # check for suite 2P data
-            self._check_for_suite2P_data()
-
-            # check for other sessions that 2P data is aligned to
-            self._check_for_coaligned_suite2p_sessions()
+        # # check for VR data
+        # if self.vr_filename is None:
+        #     self._check_for_VR_data()
+        #
+        # if not self.VR_only:
+        #     # check for raw 2P data
+        #     self._check_for_2P_data()
+        #
+        #     # check for suite 2P data
+        #     self._check_for_suite2P_data()
+        #
+        #     # check for other sessions that 2P data is aligned to
+        #     self._check_for_coaligned_suite2p_sessions()
 
         # print available fields
         # self.print_session_info()
