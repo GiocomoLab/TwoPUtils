fix broadcasting

spatial_analyses.py
@@ -70,7 +70,8 @@ def trial_matrix(arr, pos, tstart_inds, tstop_inds, bin_size=10, min_pos = 0,
         # while np.isnan(trial_mat).sum()>0:
         for trial in range(trial_mat.shape[0]):
             _trial_mat = np.squeeze(trial_mat[trial,:,:])
-            _trial_mat[np.isnan(_trial_mat)] = np.nanmean(_trial_mat,axis=0)
+            nan_inds = np.isnan(_trial_mat[trial,:,cell])
+            _trial_mat[nan_inds,:] = np.nanmean(_trial_mat,axis=0,keepdims=True)
             trial_mat[trial,:,:]=_trial_mat
             # trial_mat[np.isnan(trial_mat[:,:,cell]),cell] =np.nanmean(trial_mat[:,:,cell],axis=1)
             # naninds = np.argwhere(np.isnan(trial_mat))