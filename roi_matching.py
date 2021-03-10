c1067b4a7e0b6f1ffbc8a45f485112ce20423aa1
markplitt <markplitt@gmail.com>
2021-03-09 17:36:36 -0800

    add ROIAligner class to roi_matching module. Still needs documentation

--- roi_matching.py
+++ roi_matching.py
@@ -1,37 +1,198 @@
 import os
 import math
+from functools import reduce
 
 import numpy as np
 import scipy as sp
 from matplotlib import pyplot as plt
 
-import suite2p as s2p
-from suite2p.registration import bidiphase, utils, rigid, nonrigid
+from suite2p.registration import bidiphase, rigid, nonrigid
+from suite2p.registration import utils as reg_utils
 
 
-def align_roistack(roistack, rigid_offsets, nonrigid_offsets, ops):
-    """
+class ROIAligner:
 
-    :param roistack:
-    :param rigidOffsets:
-    :param nonrigidOffsets:
-    :param ops:
-    :return:
-    """
+    def __init__(self, ref_sess, targ_sess):
+        """
 
-    ymax, xmax = rigid_offsets
-    ymax1, xmax1 = nonrigid_offsets
-    for roi in range(roistack.shape[0]):
-        roistack[roi, :, :] = rigid.shift_frame(frame=roistack[roi, :, :], dy=ymax, dx=xmax)
+        :param ref_sess:
+        :param targ_sess:
+        """
 
-        roistack[roi, :, :] = nonrigid.transform_data(
-            data=roistack[roi:roi + 1, :, :, ],
-            nblocks=ops['nblocks'],
-            xblock=ops['xblock'],
-            yblock=ops['yblock'],
-            ymax1=ymax1,
-            xmax1=xmax1,
-        )
+        self.ref_sess = ref_sess
+        if isinstance(targ_sess, list) or isinstance(targ_sess, tuple):
+            self.targ_sess = targ_sess
+        else:
+            self.targ_sess = (targ_sess)
+
+        self.ref_roistack = TwoPUtils.roi_matching.make_roistack(self.ref_sess.s2p_stats, self.ref_sess.s2p_ops)
+
+        self.ref_match_inds = [[]] * len(self.targ_sess)
+        self.targ_match_inds = [[]] * len(self.targ_sess)
+
+        self.align_mean_images()
+
+    def run_pairwise_matches(self):
+        """
+
+        :return:
+        """
+
+        for i in range(len(self.targ_sess)):
+            self.match_session_pair(i)
+
+    def align_mean_images(self):
+        """
+
+        :return:
+        """
+
+        # get mean images
+        self.frames = np.array([s.s2p_ops['meanImg'] for s in self.targ_sess]).astype(np.float32)
+        # align them
+        self.frames, self.rigidOffsets, self.nonrigidOffsets = TwoPUtils.roi_matching.align_stack(
+            self.ref_sess.s2p_ops['meanImg'], self.frames, self.ref_sess.s2p_ops)
+
+    def match_session_pair(self, index, plot_iou_hist=True):
+        """
+
+        :param index:
+        :param plot_iou_hist:
+        :return:
+        """
+
+        targ_roistack = make_roistack(self.targ_sess[index].s2p_stats,
+                                                             self.targ_sess[index].s2p_ops)
+
+        # apply transform to roi masks
+        self.align_roistack(targ_roistack, [self.rigidOffsets[0][index], self.rigidOffsets[1][index]],
+                                              [self.nonrigidOffsets[0][index:index + 1, :],
+                                               self.nonrigidOffsets[1][index:index + 1, :]],
+                                              self.ref_sess.s2p_ops)
+
+        # calculate com from roistack
+        com_ref = get_com_from_roistack(self.ref_roistack)
+        com_targ = get_com_from_roistack(targ_roistack)
+
+        # reduce candidate matches for speed
+        dist = np.linalg.norm(com_ref[:, np.newaxis, :] - com_targ[np.newaxis, :, :], ord=2, axis=-1)
+        candidates = dist < 10
+
+        # calculate intersection over union for candidate matches
+        iou = self.iou(self.ref_roistack, targ_roistack, candidates)
+
+        # calculate iou threshold
+        thresh = self.set_iou_threshold(iou)
+
+        # get matched rois
+        ref_match_inds, targ_match_inds = self.get_matches(iou, thresh)
+        self.ref_match_inds[index], self.targ_match_inds[index] = ref_match_inds, targ_match_inds
+
+    @property
+    def common_rois_all_sessions(self):
+        """
+
+        :return:
+        """
+
+        # find cells that are in match list each time
+        ref_common_rois = reduce((lambda x, y: set(x) & set(y)), self.ref_match_inds)
+
+        common_roi_mapping = {}
+        for i, roi in enumerate(ref_common_rois):
+            print(i, roi)
+            self.common_roi_mapping[roi] = []
+            for j, (ref_list, targ_list) in enumerate(zip(sa.ref_match_inds, sa.targ_match_inds)):
+                print(j)
+                ind = np.argwhere(ref_list == roi)[0]
+                assert ind.shape[0] == 1, "duplicate matched rois somewhere"
+
+                common_roi_mapping[roi].append(targ_list[ind[0]])
+
+        return common_roi_mapping
+
+    @staticmethod
+    def align_roistack(roistack, rigid_offsets, nonrigid_offsets, ops):
+        """
+
+        :param roistack:
+        :param rigidOffsets:
+        :param nonrigidOffsets:
+        :param ops:
+        :return:
+        """
+
+        ymax, xmax = rigid_offsets
+        ymax1, xmax1 = nonrigid_offsets
+        for roi in range(roistack.shape[0]):
+            roistack[roi, :, :] = rigid.shift_frame(frame=roistack[roi, :, :], dy=ymax, dx=xmax)
+
+            roistack[roi, :, :] = nonrigid.transform_data(
+                data=roistack[roi:roi + 1, :, :, ],
+                nblocks=ops['nblocks'],
+                xblock=ops['xblock'],
+                yblock=ops['yblock'],
+                ymax1=ymax1,
+                xmax1=xmax1,
+            )
+
+    @staticmethod
+    def iou(ref_roistack, targ_roistack, candidates):
+        """
+
+        :param ref_roistack:
+        :param targ_roistack:
+        :param candidates:
+        :return:
+        """
+
+        ref_roistack_f = np.reshape(ref_roistack, [ref_roistack.shape[0], -1])
+        targ_roistack_f = np.reshape(targ_roistack, [targ_roistack.shape[0], -1])
+        iou = np.zeros([ref_roistack.shape[0], targ_roistack.shape[0]])
+        for i in range(ref_roistack.shape[0]):
+            intxn = (ref_roistack_f[i:i + 1, :] * targ_roistack_f[candidates[i, :], :]).sum(axis=-1)
+            union = (ref_roistack_f[i:i + 1, :] + targ_roistack_f[candidates[i, :], :]).sum(axis=-1)
+            iou[i, candidates[i, :]] = intxn / union
+        return iou
+
+    @staticmethod
+    def set_iou_threshold(iou):
+        """
+
+        :param iou:
+        :return:
+        """
+
+        _iou = np.copy(iou)
+        # find max across columns for each row and set it to 0
+        _iou[:, np.argmax(_iou, axis=1)] = 0
+        # find max again (2nd largest value per row)
+        # set thresh to this number
+        thresh = np.amax(_iou) + 1E-3
+        return thresh
+
+    @staticmethod
+    def get_matches(iou, thresh):
+        """
+
+        :param iou:
+        :param thresh:
+        :return:
+        """
+
+        row_sort, col_sort = np.unravel_index(np.argsort(iou, axis=None), iou.shape)
+        matched_ref, matched_targ = [], []
+
+        for i, j in zip(row_sort[::-1], col_sort[::-1]):
+            if (i not in matched_ref) and (j not in matched_targ) and (iou[i, j] > thresh):
+                matched_ref.append(i)
+                matched_targ.append(j)
+
+        return matched_ref, matched_targ
+
+
+
+
 
 
 def align_stack(ref_img, frames, ops):
@@ -76,7 +237,7 @@ def align_stack(ref_img, frames, ops):
 
     fsmooth = frames.copy().astype(np.float32)
     if ops['smooth_sigma_time'] > 0:
-        fsmooth = utils.temporal_smooth(data=fsmooth, sigma=ops['smooth_sigma_time'])
+        fsmooth = reg_utils.temporal_smooth(data=fsmooth, sigma=ops['smooth_sigma_time'])
 
     # rigid registration
     if ops.get('norm_frames', False):
